e2cd92df193a547ec831520ced9ebfffccdd71c7
Simen Fossnes <simenf13@student.uia.no>
2019-05-18 20:05:20 +0200

    Added routing.

--- UIs/store/src/App.js
+++ UIs/store/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Catalog from './screens/Catalog';
+import Cito from './screens/Cito';
 
 function App() {
   return (
     <div className="App">
-      cito store
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Catalog} />
+          <Route path="/cito/:citoId" component={Cito} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
