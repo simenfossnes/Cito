31ab8d9cc67e0b02ff90d11da77eca5fbd7d8a2f
Neža Đukić <neza.djukic@gmail.com>
2019-05-19 10:27:51 +0200

    catalog

--- UIs/store/src/screens/Catalog/Catalog.jsx
+++ UIs/store/src/screens/Catalog/Catalog.jsx
@@ -1,17 +1,24 @@
 import React, { PureComponent } from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import './Catalog.styles.css';
 
 import icon1 from '../../images/main-app-icon.png';
+import icon2 from '../../images/icons-02.png';
+import icon3 from '../../images/icons-03.png';
+import icon4 from '../../images/icons-04.png';
+import icon5 from '../../images/icons-05.png';
+import icon6 from '../../images/icons-06.png';
+import icon7 from '../../images/icons-07.png';
 
 class AppBase extends PureComponent {
 
   render() {
     return <div style={{width: "100%", height: "100%"}} align="center" >
-            <div className="module" style={{"background": `url(${icon1})`, "backgroundSize": "contain"}}>
+            <div className="module" style={{"background": `url(${this.props.img})`, "backgroundSize": "contain"}}>
               <header>
                 <h1>
-                  FacePalm
+                  {this.props.name}
                 </h1>
               </header>
             </div>
@@ -22,37 +29,15 @@ class AppBase extends PureComponent {
 class Grid1 extends PureComponent {
 
   render() {
+
+    let apps = [{id: "2", name: "PowerBank", img: icon2}, {id: "3", name: "Help", img: icon3}, {id: "4", name: "RoboTaxi", img: icon4}, {id: "5", name: "StreetView", img: icon5}, {id: "6", name: "Computer", img: icon6}, {id: "7", name: "Delivery", img: icon7}];
+
     return <div className="grid_unequal">
             <div className="item_1 items_all">
-              <AppBase />
+              <Link to="/cito/1" ><AppBase name="Wanted" img={icon1}/></Link>
             </div>
-            <div className="item_2 items_all"><AppBase /></div>
-            <div className="item_3 items_all"><AppBase /></div>
-          </div>;
-  }
-}
-
-class Grid2 extends PureComponent {
-
-  render() {
-    return <div className="grid_equal">
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
-            <div className="items_all"><AppBase /></div>
+            {apps.map((app) => 
+            <div className="items_all"><Link to={"/cito/" + app.id} ><AppBase name={app.name} img={app.img} /></Link></div>)}
           </div>;
   }
 }
@@ -102,9 +87,6 @@ class Catalog extends PureComponent {
     return (
       <div className="CatalogWrapper" style={{padding: "3vw", height:"100%", overflow: "scroll"}}>
         <Grid1 />
-        <div style={{paddingTop: "2vw"}}>
-          <Grid2 />
-        </div>
       </div>
     );
   }
